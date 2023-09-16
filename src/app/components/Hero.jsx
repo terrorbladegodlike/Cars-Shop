7815142184853c4536538c8a8b430a650c5e686c
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-09-16 16:15:32 +0300

    Variants
    "Framer-Motion" animation has been added. Created "variants.js" file with description of animation of elements appearance.

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -10,6 +10,13 @@ import Search from './Search'
 // Search Context
 import { SearchContext } from '../context/search'
 
+// Motion
+import { motion, easeInOut } from 'framer-motion'
+
+// Variants
+import { fadeIn } from '../../../variants'
+
+
 const Hero = () => {
   const { searchActive } = useContext(SearchContext)
   return (
@@ -19,16 +26,34 @@ const Hero = () => {
         <div className='flex flex-col xl:flex-row justify-center items-center xl:justify-start h-full'>
           {/* Text */}
           <div className='text-center xl:max-w-xl xl:text-left mt-16 xl:mt-0'>
-            <h1 className='h1'>
+            <motion.h1
+              variants={fadeIn('down', 0.2)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.6 }}
+              className='h1'
+            >
               Explore the Finest
               <span className='text-accent-default'> Global </span>
               Offers
-            </h1>
-            <p className='description max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10'>
+            </motion.h1>
+            <motion.p
+              variants={fadeIn('down', 0.4)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.6 }}
+              className='description max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10'
+            >
               Find your ideal ride for any adventure with our diverse range of affordable and dependable car rentals.
-            </p>
+            </motion.p>
             {/* BTN */}
-            <div className='flex gap-x-3 justify-center xl:justify-start'>
+            <motion.div
+              variants={fadeIn('down', 0.6)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.8 }}
+              className='flex gap-x-3 justify-center xl:justify-start'
+            >
               {/* Button AppStore */}
               <button className='btn-cta'>
                 <Image
@@ -47,10 +72,16 @@ const Hero = () => {
                   src={'/icons/buttons/google-play.svg'}
                 />
               </button>
-            </div>
+            </motion.div>
           </div>
           {/* Image Car */}
-          <div className='relative w-full h-full max-h-[50vh] md:max-w-[70vw] xl:max-w-[860px] xl:max-h-[542px] xl:absolute xl:right-[100px] min-[1680px]:right-[120px] xl:top-48'>
+          <motion.div
+            variants={fadeIn('down', 0.6)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.6 }}
+            className='relative w-full h-full max-h-[50vh] md:max-w-[70vw] xl:max-w-[860px] xl:max-h-[542px] xl:absolute xl:right-[100px] min-[1680px]:right-[120px] xl:top-48'
+          >
             <Image
               src={'/images/hero/car.svg'}
               fill
@@ -58,16 +89,28 @@ const Hero = () => {
               priority
               alt=''
             />
-          </div>
+          </motion.div>
         </div>
       </div>
       {searchActive ? (
-        <div className='fixed top-[80px] z-10 w-full max-w-[1920px]'>
+        <motion.div
+          initial={{ y: '-100%', }}
+          animate={{ y: 0 }}
+          transition={{ ease: easeInOut }}
+          className='fixed top-[80px] z-10 w-full max-w-[1920px]'
+        >
           <Search />
-        </div>
+        </motion.div>
       ) : (
         <div className='-mt-12 w-full max-w-[1300px] mx-auto'>
-          <Search />
+          <motion.div
+            variants={fadeIn('up', 0.8)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.2 }}
+          >
+            <Search />
+          </motion.div>
         </div>
       )}
     </section>
