9971cfe777c846b94cb2de99633c822661a2ecbc
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-09-17 11:08:54 +0300

    Added "CTA" Component
    Using: next-image, framer-motion.

--- src/app/components/CTA.jsx
+++ src/app/components/CTA.jsx
@@ -1,8 +1,88 @@
+'use client'
 import React from 'react'
+
+// Next Image
+import Image from 'next/image'
+
+// Framer Motion
+import { motion } from 'framer-motion'
+
+// Vatiants
+import { fadeIn } from '../../../variants'
+
+
 
 const CTA = () => {
   return (
-    <section className='pt-24 xl:pt-48 flex items-end pb-0 bg-[#b2b7c2]/10 bg' id='contact'>CTA</section>
+    <section className='pt-24 xl:pt-48 flex items-end pb-0 bg-[#b2b7c2]/10' id='contact'>
+      <div className="container mx-auto">
+        <div className='flex flex-col md:flex-row xl:items-center'>
+          {/* Text */}
+          <div className='flex-1 xl:ml-24 text-center md:text-left mb-12 md:mb-0'>
+            <div className='max-w-[520px] mx-auto order-2 xl:order-none'>
+              <motion.h2
+                variants={fadeIn('right', 0.2)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{ once: false, amount: 0.6 }}
+                className='h2'
+              >
+                Download our App now and hit the road with ease
+              </motion.h2>
+              <motion.p
+                variants={fadeIn('right', 0.4)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{ once: false, amount: 0.6 }}
+                className='mb-10'
+              >
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto itaque fugit tempore, corporis voluptates magnam nobis amet ducimus temporibus consequatur.
+              </motion.p>
+              {/* BTN */}
+              <motion.div
+                variants={fadeIn('right', 0.6)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{ once: false, amount: 0.4 }}
+                className='flex gap-x-3 justify-center md:justify-start'
+              >
+                <button className='btn-cta'>
+                  <Image
+                    src={'/icons/buttons/google-play.svg'}
+                    width={132}
+                    height={36}
+                    alt=''
+                  />
+                </button>
+                <button className='btn-cta'>
+                  <Image
+                    src={'/icons/buttons/app-store.svg'}
+                    width={132}
+                    height={36}
+                    alt=''
+                  />
+                </button>
+              </motion.div>
+            </div>
+          </div>
+          {/* Image */}
+          <motion.div
+            variants={fadeIn('up', 0.8)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.2 }}
+            className='flex-1 flex justify-center order-1 md:order-none'
+          >
+            <Image
+              src={'/images/cta/phone.svg'}
+              width={320}
+              height={640}
+              alt=''
+            />
+          </motion.div>
+        </div>
+      </div>
+    </section >
   )
 }
 
